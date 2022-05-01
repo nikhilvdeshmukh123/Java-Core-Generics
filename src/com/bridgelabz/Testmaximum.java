8c171123cb4f39b7014de4d27b195234ad3c0078
nikhilvdeshmukh <deshmukhnikhil81@gmail.com>
2022-05-02 02:15:02 +0530

    Added: Max Method TO print Max

--- src/com/bridgelabz/Testmaximum.java
+++ src/com/bridgelabz/Testmaximum.java
@@ -2,17 +2,18 @@ package com.bridgelabz;
 
 public class Testmaximum<T extends Comparable<T>> {
 	private T[] intArray;
-
 	Testmaximum(T[] intArray) {
 		this.intArray = intArray;
 	}
-
 	public void maxElement() {
 		for (int i = 0; i < intArray.length - 1; i++) {
 			if (intArray[i].compareTo(intArray[i + 1]) > 0) {
 				intArray[i + 1] = intArray[i];
 			}
 		}
+		printMax();
+	}
+	private void printMax() {
 		System.out.println("Maximum " + intArray[intArray.length - 1]);
 	}
 }
